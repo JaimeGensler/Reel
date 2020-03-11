ad35c908289144b1bdea9559dc04cceb3315f2b1
JaimeGensler <jaimegensler0@gmail.com>
2020-03-11 14:24:38 -0700

    Add ClientOnly rendering component

---
+++ components/ClientOnly.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+
+export default function ClientOnly({ children }) {
+    const [shouldRender, setShouldRender] = useState(false);
+
+    useEffect(() => setShouldRender(true), []);
+
+    if (!shouldRender) return null;
+    return children;
+}
