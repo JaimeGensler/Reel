5814e22b4db0ea2f85ab380f0307bbe43b801bd5
JaimeGensler <jaimegensler0@gmail.com>
2020-03-12 10:06:13 -0700

    Change find to findOne when querying database for user

--- pages/api/login.js
+++ pages/api/login.js
@@ -5,23 +5,17 @@ export default async (req, res) => {
     if (req.method === 'POST') {
         const { username, password } = req.body;
 
-        const user = await User.find({
+        const user = await User.findOne({
             username,
             passwordHash: password,
         });
 
-        if (user.length === 1) {
-            const token = jwt.sign(
-                { ...user[0]._doc },
-                process.env.JWT_SECRET,
-                {
-                    expiresIn: '5m',
-                }
-            );
+        if (user) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+                expiresIn: '10m',
+            });
+
             res.status(200).json({ token });
-        } else if (user.length > 1) {
-            res.status(500).end();
-            console.log('Multiple users returned from database!');
         } else {
             res.status(401).json({
                 message: 'No matching username & password found',
