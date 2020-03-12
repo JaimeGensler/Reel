7221c885329ed9c096278e12bd86d48a5b8916c0
JaimeGensler <jaimegensler0@gmail.com>
2020-03-12 09:23:04 -0700

    Add function name to home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import Layout from '../components/Layout';
 
-export default function() {
+export default function Home() {
     return (
         <Layout tabTitle="Reel - Home">
             <h1>Front Page</h1>
