a16dc36ef17adcbe95b1483b0977954c1b5765e4
Jaime Gensler <jaimegensler0@gmail.com>
2020-05-05 15:16:07 -0700

    Convert to TypeScript

--- components/LogIn/Form.tsx
+++ components/LogIn/Form.tsx
@@ -1,14 +1,30 @@
-import { Form, Input, Button } from 'antd';
+import { Form, Input, Button } from 'antd/lib/index';
+import { FormProps } from 'antd/lib/form/Form';
 import styled from 'styled-components';
 
 const { Item } = Form;
+const ButtonItem = styled(Item)`
+    margin-bottom: 0;
+`;
 const Submit = styled(Button)`
     float: right;
 `;
 
-export default function SignIn({ handleSubmit, loading }) {
+interface LogInValues {
+    username: string;
+    password: string;
+}
+type Props = {
+    handleSubmit: (values: LogInValues) => void;
+    loading: boolean;
+};
+export default function SignIn({ handleSubmit, loading }: Props) {
     return (
-        <Form onFinish={handleSubmit} layout="vertical" hideRequiredMark>
+        <Form
+            onFinish={handleSubmit as FormProps['onFinish']}
+            layout="vertical"
+            hideRequiredMark
+        >
             <Item
                 name="username"
                 label="Username"
@@ -18,6 +34,7 @@ export default function SignIn({ handleSubmit, loading }) {
             >
                 <Input placeholder="Username" disabled={loading} />
             </Item>
+
             <Item
                 name="password"
                 label="Password"
@@ -27,11 +44,17 @@ export default function SignIn({ handleSubmit, loading }) {
             >
                 <Input.Password placeholder="Password" disabled={loading} />
             </Item>
-            <Item style={{ marginBottom: 0 }}>
-                <Submit type="primary" htmlType="submit" loading={loading}>
+
+            <ButtonItem>
+                <Submit
+                    type="primary"
+                    htmlType="submit"
+                    loading={loading}
+                    disabled={loading}
+                >
                     Sign{loading ? 'ing' : ''} In{loading ? '...' : ''}
                 </Submit>
-            </Item>
+            </ButtonItem>
         </Form>
     );
 }
