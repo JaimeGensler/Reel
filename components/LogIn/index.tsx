3678d55eb9aed5c597ca19d0598a56c956033cb9
Jaime Gensler <jaimegensler0@gmail.com>
2020-05-05 14:31:28 -0700

    Fix types

--- components/LogIn/index.tsx
+++ components/LogIn/index.tsx
@@ -10,43 +10,44 @@ const Wrapper = styled.div`
     width: 100%;
     height: calc(100vh - 64px);
     padding-top: 4rem;
-
     background-color: #fafafa;
 `;
-
 const Content = styled.div`
     width: 19rem;
     padding: 2rem;
     margin: 0 auto;
-
     background-color: white;
     border-radius: 2px;
     border: 1px solid #8f8f8f;
 `;
 
+interface LogInValues {
+    username: string;
+    password: string;
+}
 export default function Index() {
     const [loading, setLoading] = useState(false);
-    const [infoMessage, setInfoMessage] = useState(null);
+    const [infoMessage, setInfoMessage] = useState(0);
 
     useEffect(() => {
         if (Router.query.redirected_from) setInfoMessage(1);
     }, []);
 
-    const onSubmit = values => {
+    const onSubmit = (values: LogInValues) => {
         setLoading(true);
         axios
             .post('/api/login', values)
-            .then(res => {
+            .then((res) => {
                 localStorage.setItem('reel:token', res.data.token);
-                Router.push(Router.query.redirected_from || '/');
+                Router.push((Router.query.redirected_from as string) || '/');
             })
-            .catch(err => setInfoMessage(err.response.status))
+            .catch((err) => setInfoMessage(err.response.status))
             .finally(() => setLoading(false));
     };
 
     return (
         <Wrapper>
-            <Info code={infoMessage} handleClose={e => setInfoMessage(null)} />
+            <Info code={infoMessage} handleClose={() => setInfoMessage(0)} />
             <Content>
                 <Heading>Sign In</Heading>
                 <Form handleSubmit={onSubmit} loading={loading} />
