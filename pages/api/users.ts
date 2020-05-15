2763c0d0cb1000600004129983cb0bf6ea478dad
Jaime Gensler <jaimegensler0@gmail.com>
2020-05-15 12:53:48 -0700

    Remove unused API route

--- pages/api/users.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import User from '../../models/User';
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-    const users = await User.find();
-    res.status(200).json(users);
-};
