aec67e08f091f259356f22271a582696997c559d
Jaime Gensler <jaimegensler0@gmail.com>
2020-05-14 17:48:07 -0700

    Add Fauna connection to env config

--- next.config.js
+++ next.config.js
@@ -4,5 +4,6 @@ module.exports = {
     env: {
         CONNECT_DATABASE: process.env.CONNECT_DATABASE,
         JWT_SECRET: process.env.JWT_SECRET,
+        FAUNA_DB: process.env.FAUNA_DB,
     },
 };
