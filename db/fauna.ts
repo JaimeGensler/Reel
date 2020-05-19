2ead1c0f633475918ebd5d0e871ec6f1816d57ef
Jaime Gensler <jaimegensler0@gmail.com>
2020-05-19 14:27:36 -0700

    Remove FaunaDB Driver from packages

--- db/fauna.ts
+++ db/fauna.ts
@@ -1,11 +1,3 @@
-import faunadb from 'faunadb';
-
-export const q = faunadb.query;
-export const client = new faunadb.Client({
-    secret: process.env.FAUNA_DB as string,
-    timeout: 5000,
-});
-
 export const queryURL = 'https://graphql.fauna.com/graphql';
 export const faunaHeaders = {
     headers: {
